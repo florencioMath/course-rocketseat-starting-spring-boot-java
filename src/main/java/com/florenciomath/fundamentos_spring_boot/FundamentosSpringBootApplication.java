f93b3859ea314de471077f41bf6bfc43fcbed6f9
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-04-07 20:20:26 -0300

    Initial commit.

---
+++ src/main/java/com/florenciomath/fundamentos_spring_boot/FundamentosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.florenciomath.fundamentos_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringBootApplication.class, args);
+	}
+
+}
