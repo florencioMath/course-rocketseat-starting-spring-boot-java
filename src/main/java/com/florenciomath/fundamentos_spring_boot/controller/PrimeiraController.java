0016a35715b9a57f111e1f9d32b0c712698ee35a
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-04-09 20:42:59 -0300

    feat: Aula: Path Params - Query Params.

--- src/main/java/com/florenciomath/fundamentos_spring_boot/controller/PrimeiraController.java
+++ src/main/java/com/florenciomath/fundamentos_spring_boot/controller/PrimeiraController.java
@@ -2,6 +2,8 @@ package com.florenciomath.fundamentos_spring_boot.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
@@ -9,6 +11,16 @@ public class PrimeiraController {
     @GetMapping("/pathParams/{id}")
     public String pathParams(@PathVariable String id) {
         return "A Path Variable é: " + id;
+    }
+
+    @GetMapping("/queryParam")
+    public String queryParam(@RequestParam String id) {
+        return "A Query Param é: " + id;
+    }
+
+    @GetMapping("/queryParams")
+    public String queryParams(@RequestParam Map<String, String> allParams) {
+        return "As Query Params são: " + allParams;
     }
 
 }
